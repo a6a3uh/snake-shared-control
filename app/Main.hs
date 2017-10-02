5a284d937dac14ee17828ecf2191370eae078272
Ali Tlisov <ali.tlisov@gmail.com>
2017-10-02 11:39:55 +0300

    fixed names in yaml adding modifiers to parser

--- app/Main.hs
+++ app/Main.hs
@@ -21,7 +21,8 @@ import Draw
 data CostSettings = NewCostSettings
     { _function :: String
     , _scale :: Double 
-    } deriving (Generic, Show)
+    } deriving  ( Generic
+                , Show)
 
 data GameSettings1 = NewGameSettings1
     { _auto :: Bool
@@ -32,29 +33,34 @@ data GameSettings1 = NewGameSettings1
     , _dimentions :: (Int, Int)
     , _pixels :: (Int, Int)
     , _cost :: CostSettings
-    } deriving (Generic, Show)
+    } deriving  ( Generic
+                , Show)
 
 data SnakeSettings = NewSnakeSettings
     { _size1 :: Int
     , _position :: (Int, Int)
-    } deriving (Generic, Show)
+    } deriving  ( Generic
+                , Show)
 
 data FoodSettings = NewFoodSettings
     { _number :: (Int, Int)
     , _reward1 :: (Int, Int)
     , _exact :: FoodData
-    } deriving (Generic, Show)
+    } deriving  ( Generic
+                , Show)
 
 data FoodData = NewFoodData
     { _positions :: [(Int, Int)]
     , _costs :: [Int]
-    } deriving (Generic, Show)
+    } deriving  ( Generic
+                , Show)
 
 data Settings1 = NewSettings1
     { _game :: GameSettings1
     , _snake1 :: SnakeSettings
     , _food :: FoodSettings
-    } deriving (Generic, Show)
+    } deriving  ( Generic
+                , Show)
 
 makeLenses ''CostSettings
 makeLenses ''GameSettings1
@@ -69,22 +75,23 @@ instance FromJSON CostSettings where
 
 instance FromJSON Settings1 where
     parseJSON = genericParseJSON defaultOptions {
-                    fieldLabelModifier = drop 1 }
+                    fieldLabelModifier = \s -> if s == "_snake1" then  "snake" else drop 1 s }
 
 instance FromJSON FoodData where
     parseJSON = genericParseJSON defaultOptions {
-                    fieldLabelModifier = drop 1 }
+                    fieldLabelModifier = drop 1}
+
 instance FromJSON FoodSettings where
     parseJSON = genericParseJSON defaultOptions {
-                    fieldLabelModifier = drop 1 }
+                    fieldLabelModifier = \s -> if s == "_reward1" then "reward" else drop 1 s }
 
 instance FromJSON SnakeSettings where
     parseJSON = genericParseJSON defaultOptions {
-                    fieldLabelModifier = drop 1 }
+                    fieldLabelModifier = \s -> if s == "_size1" then "size" else drop 1 s}
+
 instance FromJSON GameSettings1 where
     parseJSON = genericParseJSON defaultOptions {
-                    fieldLabelModifier = drop 1 }
-
+                    fieldLabelModifier = drop 1}
                         
 main :: IO ()
 main = do
@@ -98,7 +105,7 @@ main = do
             NewGameWorld {
                 _snakeWorld = NewWorld
                 { _direction = North
-                , _snake = [(0, 0)]
+                , _snake = [config ^. snake1 . position]
                 , _stomack = config ^. snake1 . size1
                 , _isOver = False
                 , _gen = mkStdGen seed
@@ -112,7 +119,6 @@ main = do
                 } 
             , _resolution = config ^. game . pixels
         }
-        -- -- = initialGameWorld seed
 
     play
         (displayMode gworld)
